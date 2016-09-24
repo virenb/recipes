c573990aa468b783c94b22e2b6a795177614bb67
Viren Bhagat <virenb@gmail.com>
2016-09-24 15:37:02 -0400

    Updated updated main.js to retrieve results

--- public/main.js
+++ public/main.js
@@ -1,13 +1,66 @@
 $(function() {
 
-    $('#submit').on('click', function(e) {
-        e.preventDefault();
-        var ingred = $('#ingredient').val();
-        $('ul').append('<li>' + ingred + ' <input type="checkbox"></li>');
-        $('#ingredient').val('');
+
+  $('#enter').on('click', function(e) {
+    e.preventDefault();
+    var ingred = $('#ingredient').val();
+    $('#list-group').append('<li class="list-group-item">' + ingred + ' <input type="checkbox" /> <input type="hidden" name="ingredients[]" value="' + ingred + '" /></li>');
+    $('#ingredient').val('');
+    var data = $("#ing").serializeArray();
+    var dataString = JSON.stringify(data);
+    //console.log(dataString);
+    var ingredientsMap = $.map(data, function(u) {
+      return u.value;
+    });
+    var joinMap = ingredientsMap.join(',');
+    //console.log(joinMap);
+
+  });
+
+  $("#delete").on("click", function() {
+    $("input:checked").closest(".list-group-item").remove();
+    var data = $("#ing").serializeArray();
+    var dataString = JSON.stringify(data);
+    //console.log(deletedDataString);
+    var ingredientsMap = $.map(data, function(u) {
+      return u.value;
+    });
+    var joinMap = ingredientsMap.join(',');
+    //console.log(joinMap);
+  });
+
+  var searchRecipes = function() {
+    var data = $("#ing").serializeArray();
+    var dataString = JSON.stringify(data);
+    var ingredientsMap = $.map(data, function(u) {
+      return u.value;
+    });
+    var joinMap = ingredientsMap.join(',');
+    //console.log(joinMap);
+
+    var request = $.ajax({
+      url: "https://spoonacular-recipe-food-nutrition-v1.p.mashape.com/recipes/findByIngredients?ingredients=" + joinMap + "&number=8&ranking=1",
+      type: "GET",
+      dataType: "json",
+      headers: {
+        "X-Mashape-Key": "FpwFL1xHGUmshSg3BCsL4xQxsDrzp1SNzyTjsnqDwPVsW7Hk89"
+      }
     });
 
-    $("#delete").on("click", function() {
-        $("input:checked").parent().remove();
+    request.done(function(data) {
+      // $("#results").text(JSON.stringify(data));
+      $.each(data, function (index, value) {
+        $("#results").append(value.title + "<br /><img src='" + value.image + "' alt='' width='100' height='100' />");
+      });
     });
+
+    request.fail(function(jqXHR, textStatus) {
+      alert("Request failed: " + textStatus);
+    });
+  };
+  $('#submit').on('click', function(e) {
+    e.preventDefault();
+    console.log("Hey");
+    searchRecipes();
+  });
 });
