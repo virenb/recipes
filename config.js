626694bb70847d95d8577771e4fda9fd573d85bd
Viren Bhagat <virenb@gmail.com>
2016-09-27 13:07:05 -0400

    Updated port variable in config.js

--- config.js
+++ config.js
@@ -4,7 +4,7 @@ exports.DATABASE_URL = process.env.DATABASE_URL ||
                             'mongodb://virenb:hakeem3343@ds041496.mlab.com:41496/recipes-node-cap':
                             'mongodb://virenb:hakeem3343@ds041496.mlab.com:41496/recipes-node-cap');
 
-var port = process.env.PORT || 8080;              
+var port = process.env.PORT || 3000;              
 exports.port = port
 
 
